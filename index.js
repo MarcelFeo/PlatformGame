0cf4c7162728695fd55c43cb6dcbd03539d2dbfc
MarcelFeo <marcelfeo29@gmail.com>
2022-08-11 12:18:15 -0300

    Create Player

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+
+class Player {
+    constructor() {
+        this.position = { x: 100, y: 100 }
+        this.width = 100;
+        this.height = 100;
+    };
+
+    draw() {
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const player = new Player();
