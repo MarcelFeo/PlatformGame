04fb3f37a4be130112d9f0a6e5f3e976c697e2be
MarcelFeo <marcelfeo29@gmail.com>
2022-08-11 12:37:44 -0300

    Create Animate

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+// Canvas Configuration
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+// Width, Height of screen
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+// Player Class
 class Player {
     constructor() {
-        this.position = { x: 100, y: 100 }
-        this.width = 100;
-        this.height = 100;
+        this.position = { x: 100, y: 100 };
+        this.velocity = { x: 0, y: 1 };
+        this.width = 30;
+        this.height = 30;
     };
 
     draw() {
+        context.fillStyle = "red";
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.position.y += this.velocity.y;
+        this.draw();
     }
 }
 
 const player = new Player();
+player.update();
+
+// Animation Function
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+};
+
+animate();
