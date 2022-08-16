9b5544be341befaf2b628c852394c488c9078e47
MarcelFeo <marcelfeo29@gmail.com>
2022-08-16 11:51:25 -0300

    Fine Tuning

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ function animate() {
 
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = player.speed;
-    } else if(keys.left.pressed && player.position.x > 100) {
+    } else if((keys.left.pressed && player.position.x > 100) || keys.left.pressed && scrollOffset === 0 && player.position.x > 0) {
         player.velocity.x = player.speed;
     } else {
         player.velocity.x = 0;
